135ebd8dfec8ba60209d539bf323d577115a2b7e
OwenOchieng <owen.ochieng@student.moringaschool.com>
2022-06-15 02:17:24 +0300

    commit tree a40f7701535096a99f687f28dc2f511cd79b18b8

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // Your code here
+const dodger = document.getElementById('dodger');
+
+
 function moveDodgerLeft() {
     const leftNumbers = dodger.style.left.replace("px", "");
     const left = parseInt(leftNumbers, 10);
@@ -12,13 +15,18 @@ document.addEventListener("keydown", function (e) {
     if (e.key === "ArrowLeft") {
       moveDodgerLeft();
     }
-    else if (e.key === "ArrowRight"){
-        moveDodgerRight();
-    }
-  });
+ });
 
 function moveDodgerRight() {
     const leftNumbers = dodger.style.left.replace("px", "");
     const left = parseInt(leftNumbers, 10);
     dodger.style.left = `${left + 1}px`;
 }
+
+ document.addEventListener("keydown", function(e) {
+      if (e.key === "ArrowRight") {
+          moveDodgerRight();
+      }
+  });
+  
+
